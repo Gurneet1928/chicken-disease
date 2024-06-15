952e0bc810df13ad34a92021b6362833284c01ab
Gurneet Singh <gurneet222@gmail.com>
2024-06-16 00:52:19 +0530

    Data Ingestion Completed

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to Custom Logs")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>>>>> Starting Stage: {STAGE_NAME} <<<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>>> Stage: {STAGE_NAME} Completed <<<<<<<<<\n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -10,6 +10,8 @@ from pathlib import Path
 from typing import Any
 import base64
 
+# ensure_annotations decorator is used to check the type of input arguments and output arguments
+
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:                 # Input Arguments -> Output Argument Type
     """
